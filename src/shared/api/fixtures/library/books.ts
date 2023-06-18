cdcbb907caf172ae14b274bd76fb7a489236ca60
Serega <don.ziablikov2016@yandex.ru>
2023-06-18 20:20:59 +0300

    bag: не добавил изображение))

--- src/shared/api/fixtures/library/books.ts
+++ src/shared/api/fixtures/library/books.ts
@@ -517,7 +517,6 @@ type GetListParams = {
         };
         tariff?: number;
         existsOnly?: boolean;
-        // !!! FIXME: Ужаснейший код! Никогда не показывайте это бекендерам!
         getRentInfoBy?: (book: AbstractBook) => {
             duration: number;
             status: "OUT_STOCK" | "RENTABLE" | "RESERVABLE" | "OWN";
